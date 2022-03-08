4283e44374bd62d7265d6f6bab6abee66cb545dc
Carl Fredrik Ahl <carlfredrikahl@gmail.com>
2022-03-08 13:24:35 +0100

    Can open camera

--- app/src/main/java/com/example/firsttestapp/MainActivity.java
+++ app/src/main/java/com/example/firsttestapp/MainActivity.java
@@ -2,13 +2,17 @@ package com.example.firsttestapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     Button changeActBtn;
+    TextView toolbarText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,9 +24,20 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 //Start intent
-                changeAct();
+                //changeAct();
+                openCam();
             }
         });
+    }
+
+    public void openCam() {
+        Intent takePicIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        try {
+            startActivity(takePicIntent);
+        } catch (ActivityNotFoundException e) {
+
+        }
     }
 
     public void changeAct() {
