487f432eb02ee8fcdafeb51b2c9adf1f7fbb6345
Carl Fredrik Ahl <carlfredrikahl@gmail.com>
2022-03-10 11:01:21 +0100

    Added model viewer

--- app/src/main/java/com/example/firsttestapp/MainActivity.java
+++ app/src/main/java/com/example/firsttestapp/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
@@ -13,56 +14,18 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.garmin.android.connectiq.ConnectIQ;
-import com.garmin.android.connectiq.ConnectIQ.*;
-import com.garmin.android.connectiq.IQDevice;
-
-import java.sql.Array;
 import java.util.ArrayList;
 import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity {
     Button changeActBtn;
-    TextView toolbarText;
-
-    //Garmin Connect
-    ConnectIQ connectIQ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        connectIQ = ConnectIQ.getInstance(this, IQConnectType.WIRELESS);
-        connectIQ.initialize(this, true, new ConnectIQListener() {
-            @Override
-            public void onSdkReady() {
-                Toast toast = Toast.makeText(getApplicationContext(), "Garmin SDK Success", Toast.LENGTH_LONG);
-                toast.show();
-
-                ListView listView = findViewById(R.id.listView);
-
-                List<String> list = new ArrayList<>();
-                list.add("Test 1");
-                list.add("Test 2");
-                list.add("Test 3");
-
-                ArrayAdapter arrayAdapter = new ArrayAdapter(getApplicationContext(), android.R.layout.simple_list_item_1, list);
-
-                listView.setAdapter(arrayAdapter);
-            }
-
-            @Override
-            public void onInitializeError(IQSdkErrorStatus iqSdkErrorStatus) {
-
-            }
-
-            @Override
-            public void onSdkShutDown() {
-
-            }
-        });
 
         changeActBtn = (Button) findViewById(R.id.changeActBtn);
         changeActBtn.setOnClickListener(new View.OnClickListener() {
@@ -70,20 +33,18 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 //Start intent
                 //changeAct();
-                openCam();
-                
+                modelViewer();
             }
         });
     }
 
-    public void openCam() {
-        Intent takePicIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+    public void modelViewer() {
+        Intent modelViewerIntent = new Intent(Intent.ACTION_VIEW);
+        Intent sceneViewerIntent = new Intent(Intent.ACTION_VIEW);
+        sceneViewerIntent.setData(Uri.parse("https://arvr.google.com/scene-viewer/1.0?file=https://raw.githubusercontent.com/KhronosGroup/glTF-Sample-Models/master/2.0/Avocado/glTF/Avocado.gltf"));
+        sceneViewerIntent.setPackage("com.google.android.googlequicksearchbox");
+        startActivity(sceneViewerIntent);
 
-        try {
-            startActivity(takePicIntent);
-        } catch (ActivityNotFoundException e) {
-
-        }
     }
 
     public void changeAct() {
